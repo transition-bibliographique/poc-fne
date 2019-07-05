30da12321b0ee6576809a3b06d53a3e9d4b42aeb
maxlath <k@maxlath.eu>
2019-07-05 15:55:45 +0200

    Génère le format temporaire des propriétés

--- tests/interxmarc_pep.js
+++ tests/interxmarc_pep.js
@@ -1,12 +1,18 @@
 require('should')
 const robertFlemingNotice = require('./fixtures/RobertFleming_BnF_14797579.json')
-const parse = require('../lib/transform/parse')
+const parse = require('../lib/transform/parse_interxmarc')
 
 describe('parser', () => {
   it('should return a list of used interxmarc properties', done => {
-    const propertiesList = parse(robertFlemingNotice)
+    const propertyId = 'interxmarc:031:a:0'
+    const properties = parse(robertFlemingNotice)
+    properties.should.be.an.Object()
+    const propertiesList = Object.keys(properties)
     propertiesList.should.be.an.Array()
-    propertiesList.includes('031 a').should.be.true()
+    propertiesList.includes(propertyId).should.be.true()
+    properties[propertyId].id.should.equal(propertyId)
+    properties[propertyId].aliases.fr.should.equal(propertyId)
+    properties[propertyId].datatype.should.equal('string')
     done()
   })
 })
