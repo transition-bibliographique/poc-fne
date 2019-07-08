8d2db18f063375cd9add4cccaa2322e68fcfbfb4
jums <jums@inventaire.io>
2019-07-08 16:56:17 +0200

    Force les clés 'subfield' en une liste d'élément
    dans une clé renomé esubfields'
    
    - place cette responsabilité dans l'extracteur car c'est probablement une
    spécificité de l'éditeur de notices BnF
    Le transformateur peut
    ainsi s'attendre dans tous les cas à ce que la notice ait une clé 'subfields'
    
    - adapte les fixtures en conséquence

--- scripts/extract.js
+++ scripts/extract.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const getEchantillonData = require('../lib/extract/get_echantillon_data')
 const parseRecord = require('../lib/extract/parse_record')
+const sanitizeData = require('../lib/extract/sanitize_data')
 const path = require('path')
 const fs = require('fs')
 
@@ -10,7 +11,8 @@ const filePath = path.join(__dirname, '..', fileName)
 
 const data = getEchantillonData(filePath)
 const fields = parseRecord(data)
-const json = JSON.stringify(fields)
+const sanitizeFields = sanitizeData(fields)
+const json = JSON.stringify(sanitizeFields)
 
 const jsonExportPath = filePath.split('.')[0] + '.json'
 fs.writeFileSync(jsonExportPath, json)
