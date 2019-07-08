6c3012aa37dec72ee1b6a8496a0d5096b5da0556
jums <jums@inventaire.io>
2019-07-09 01:07:22 +0200

    Créer un item avec ses claims
    Ignore l'appartenance des sous-champs à un meme champs répété.
    Par exemple, si deux champs contiennent les 2 mêmes souschamps,
    alors deux statements ayant la même propriété sont créés.
    A voir plus tard si l'appartenance de sous-champs à un même champs
    peut se modéliser dans des qualifiers par exemple.
    
    Transforme les dates en format wikibase (à faire : modifier les type de
    données dans les proprités de date)
    
    Pour l'instant les propriétés des claims ne sont que des pseudo Id (à faire : remplacer par des propriétés de wikibase une fois créées)

--- tests/interxmarc_pep.js
+++ tests/interxmarc_pep.js
@@ -1,11 +1,12 @@
 require('should')
 const robertFlemingNotice = require('./fixtures/RobertFleming_BnF_14797579.json')
-const parse = require('../lib/transform/parse_interxmarc')
+const parseProperties = require('../lib/transform/parse_properties')
+const parseItems = require('../lib/transform/parse_items')
 
-describe('parser', () => {
-  it('should return a list of used interxmarc properties', done => {
+describe('create pseudo properties from an interxmarc notice', () => {
+  it('should return an object of pseudo properties', done => {
     const propertyId = 'interxmarc:031:a:0'
-    const properties = parse(robertFlemingNotice)
+    const properties = parseProperties(robertFlemingNotice)
     properties.should.be.an.Object()
     const propertiesList = Object.keys(properties)
     propertiesList.should.be.an.Array()
@@ -13,6 +14,35 @@ describe('parser', () => {
     properties[propertyId].id.should.equal(propertyId)
     properties[propertyId].aliases.fr.should.equal(propertyId)
     properties[propertyId].datatype.should.equal('string')
+    done()
+  })
+})
+
+describe('create a pseudo item from an interxmarc notice', () => {
+  it('should return a pseudo item id', done => {
+    const itemId = '0  b.Fleming.Robert.1921-1976'
+    const item = parseItems(robertFlemingNotice)
+    item.should.be.an.Object()
+    item.id.should.equal(itemId)
+    item.aliases.fr.should.equal(itemId)
+    done()
+  })
+
+  it('should return pseudo claims', done => {
+    const itemPropertyPseudoId = 'interxmarc:031:a:0'
+    const item = parseItems(robertFlemingNotice)
+    item.claims.should.be.an.Object()
+    item.claims[itemPropertyPseudoId].should.be.an.Array()
+    item.claims[itemPropertyPseudoId][0].should.equal('0000000073689743')
+    done()
+  })
+
+  it('should return pseudo claims with date value', done => {
+    const itemPropertyPseudoId = 'interxmarc:031:d:2'
+    const item = parseItems(robertFlemingNotice)
+    item.claims.should.be.an.Object()
+    item.claims[itemPropertyPseudoId].should.be.an.Array()
+    item.claims[itemPropertyPseudoId][0].should.equal('2013-08-02')
     done()
   })
 })
