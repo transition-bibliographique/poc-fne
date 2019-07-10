af026233de1b6db83f63b9d11c032d4c72c515a3
jums <jums@inventaire.io>
2019-07-11 00:26:16 +0200

    Charge les propriétés d'une PEP dans wikibase
    Utilise la bibliothèque wikibase-edit (documenté dans packages.json)
    Configuration de wb-edit dans 'config/locale'

---
+++ tests/load_properties.js
@@ -0,0 +1,27 @@
+require('should')
+const robertFlemingNotice = require('./fixtures/RobertFleming_BnF_14797579.json')
+const parseProperties = require('../lib/transform/parse_properties')
+const loadProperties = require('../lib/load/load_properties')
+
+
+describe('load properties on wikibase', () => {
+  it('should return an object of Wikibase properties', done => {
+    const propertyId = 'interxmarc:008:3233'
+    const properties = parseProperties(robertFlemingNotice)
+    // {
+    //   'interxmarc:008:3233': {
+    //     pseudoId: 'interxmarc:008:3233',
+    //     datatype: 'string',
+    //     aliases: { fr: 'interxmarc:008:3233' }
+    //   }
+    // }
+    loadProperties(properties)
+    .then((res) => {
+      res.should.be.an.Object()
+      res[propertyId].should.be.an.Object()
+      res[propertyId].id.should.startWith('P')
+      done()
+    })
+    .catch(done)
+  })
+})
