07e0cb7cada5289755de380d3b2a34683706be32
maxlath <k@maxlath.eu>
2019-07-21 13:22:49 +0200

    Sépare les responsabilités entre les notices d'oeuvres et d'auteurs

--- test/transform_interxmarc_notice_oeuvre.js
+++ test/transform_interxmarc_notice_oeuvre.js
@@ -43,7 +43,7 @@ describe('create a pseudo item from an interxmarc work', () => {
   describe('transform datafield', () => {
     it('should return a pseudo item id', done => {
       const itemId = '.0..b.fre..Les rêveries du promeneur solitaire'
-      const items = parseNotice(lesReveriesDuPromeneur)
+      const { items } = parseNotice(lesReveriesDuPromeneur)
       const item = items[0]
       item.should.be.an.Object()
       item.pseudoId.should.equal(itemId)
@@ -54,7 +54,7 @@ describe('create a pseudo item from an interxmarc work', () => {
   describe('transform datacontrol', () => {
     it('should return pseudo claims', done => {
       const itemPropertyPseudoId = 'interxmarc:001:0001'
-      const items = parseNotice(lesReveriesDuPromeneur)
+      const { items } = parseNotice(lesReveriesDuPromeneur)
       const item = items[0]
       item.claims.should.be.an.Object()
       item.claims[itemPropertyPseudoId].should.be.an.Array()
@@ -65,11 +65,24 @@ describe('create a pseudo item from an interxmarc work', () => {
   describe('transform leader', () => {
     it('should return pseudo claims', done => {
       const itemPropertyPseudoId = 'interxmarc:000:0004'
-      const items = parseNotice(lesReveriesDuPromeneur)
+      const { items } = parseNotice(lesReveriesDuPromeneur)
       const item = items[0]
       item.claims.should.be.an.Object()
       item.claims[itemPropertyPseudoId].should.be.an.Array()
       item.claims[itemPropertyPseudoId][0].should.equal('00469')
+      done()
+    })
+  })
+
+  describe('author relation', () => {
+    it('should return an author item', done => {
+      const { items, relations } = parseNotice(lesReveriesDuPromeneur)
+      const [ workItem, authorItem ] = items
+      const authorPseudoId = '11922879..0..b......Rousseau.Jean-Jacques.1712-1778'
+      authorItem.pseudoId.should.equal(authorPseudoId)
+      relations[0].subject.should.equal(workItem.pseudoId)
+      relations[0].property.should.equal('author')
+      relations[0].object.should.equal(authorPseudoId)
       done()
     })
   })
