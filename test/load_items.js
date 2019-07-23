52dd503239e8fb6dea1b9d02953a9cb2705f2a1f
maxlath <k@maxlath.eu>
2019-07-23 18:47:36 +0200

    load_items: réusinage des tests

--- test/load_items.js
+++ test/load_items.js
@@ -11,15 +11,15 @@ describe('load items on wikibase', function () {
   it('should return a list of items', done => {
     const itemPseudoId = '.0..b.fre..Les rêveries du promeneur solitaire'
     const properties = parseProperties(lesReveriesDuPromeneur)
-    const { items } = parseNotice(lesReveriesDuPromeneur)
+    const { items, relations } = parseNotice(lesReveriesDuPromeneur)
 
     loadProperties(properties)
       .then((wbProps) => {
-        return loadItems(items, wbProps)
+        return loadItems(items, relations, wbProps)
           .then((res) => {
-            res.should.be.an.Array()
-            const item = res[0]
-            item.pseudoId.should.equal(itemPseudoId)
+            const entities = Object.values(res.entities)
+            const item = entities[0]
+            item.labels.en.value.should.equal(itemPseudoId)
             done()
           })
       })
@@ -35,18 +35,13 @@ describe('load items on wikibase', function () {
       .then((wbProps) => {
         return loadItems(items, relations, wbProps)
           .then((res) => {
-            const entities = Object.values(res.entities)
-            const workItem = entities.find(entity => {
-              return Object.values(entity.claims).find(claimProperyTypeIsWikibaseItem)
-            })
-            workItem.should.be.an.Object( )
+            const workId = res.relations[0].claim.id.split('$')[0]
+            const authorId = res.relations[0].claim.mainsnak.datavalue.value.id
+            res.entities[workId].labels.en.value.should.equal(relations[0].subject)
+            res.entities[authorId].labels.en.value.should.equal(relations[0].object)
             done()
           })
       })
       .catch(done)
   })
 })
-
-const claimProperyTypeIsWikibaseItem = propertyClaims => {
-  return propertyClaims[0].mainsnak.datatype === 'wikibase-item'
-}
