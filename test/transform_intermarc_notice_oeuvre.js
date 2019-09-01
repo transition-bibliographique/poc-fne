950a9a5cf25cd816b4dc509eaad2c2f739d40a0b
maxlath <k@maxlath.eu>
2019-09-01 23:29:52 +0200

    Modélisation de la propriété pivot "Titre de l'oeuvre" à partir d'intermarc_141, intermarc_144, intermarc_145

--- test/transform_intermarc_notice_oeuvre.js
+++ test/transform_intermarc_notice_oeuvre.js
@@ -81,6 +81,20 @@ describe('create a pseudo item from an intermarc oeuvre', () => {
   })
 
   describe('pivot property claims', () => {
+    it('should return "Titre de l\'oeuvre" claims', done => {
+      const oeuvreItem = parseNotice(sampleBNFwork).items[0]
+      oeuvreItem.claims["Titre de l'oeuvre"].should.be.an.Array()
+      const claim = oeuvreItem.claims["Titre de l'oeuvre"][0]
+      claim.value.should.equal('Les rêveries du promeneur solitaire')
+      claim.references.should.be.an.Array()
+      const reference = claim.references[0]
+      reference.should.deepEqual({
+        'identifiant de la zone': 'intermarc_145',
+        'données source de la zone': '$w .0..b.fre. $a Les rêveries du promeneur solitaire'
+      })
+      done()
+    })
+
     it('should not return pep specific claims', done => {
       const item = parseNotice(sampleBNFwork).items[0]
       should(item.claims['Nom']).not.be.ok()
