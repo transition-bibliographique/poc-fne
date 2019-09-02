ac73d94cd2ade9640c066c10b712f2d5249b4913
maxlath <k@maxlath.eu>
2019-09-02 14:05:32 +0200

    Modélisation des propriétés pivots communes aux oeuvres et personnes à partir d'Unimarc

--- test/transform_unimarc_notice_general.js
+++ test/transform_unimarc_notice_general.js
@@ -1,50 +1,50 @@
 require('should')
-const sampleBNFwork = require('./fixtures/sample_BNF_work.json')
-const sampleBNFpep = require('./fixtures/sample_BNF_pep.json')
+const sampleABESwork = require('./fixtures/sample_ABES_work.json')
+const sampleABESpersonne = require('./fixtures/sample_ABES_personne.json')
 const parseProperties = require('../lib/transform/parse_properties')
 const parseNotice = require('../lib/transform/parse_notice')
 
 describe('intermarc common pivot property claims', () => {
   it('should return "URL pérenne" claims', done => {
-    const item = parseNotice(sampleBNFwork).items[0]
+    const item = parseNotice(sampleABESwork).items[0]
     item.claims["URL pérenne"].should.be.an.Array()
     const claim = item.claims["URL pérenne"][0]
-    claim.value.should.equal('http://catalogue.bnf.fr/ark:/12148/cb11935154w')
+    claim.value.should.equal('http://www.idref.fr/082349983')
     claim.references.should.be.an.Array()
     const reference = claim.references[0]
     reference.should.deepEqual({
-      'identifiant de la zone': 'intermarc_003',
-      'données source de la zone': 'http://catalogue.bnf.fr/ark:/12148/cb11935154w'
+      'identifiant de la zone': 'unimarc_003',
+      'données source de la zone': 'http://www.idref.fr/082349983'
     })
     done()
   })
 
   it('should return "Type d\'entité" claims', done => {
-    const item = parseNotice(sampleBNFwork).items[0]
+    const item = parseNotice(sampleABESwork).items[0]
     item.claims["Type d'entité"].should.be.an.Array()
     const claim = item.claims["Type d'entité"][0]
     claim.value.should.equal('œuvre')
     claim.references.should.be.an.Array()
     const reference = claim.references[0]
     reference.should.deepEqual({
-      'identifiant de la zone': 'intermarc_000',
-      'données source de la zone': '00469c1 as22000272  45'
+      'identifiant de la zone': 'unimarc_008',
+      'données source de la zone': 'Tu5'
     })
     done()
   })
 
   it('should return "Identifiant ISNI" claims', done => {
-    const item = parseNotice(sampleBNFpep).items[0]
+    const item = parseNotice(sampleABESpersonne).items[0]
     item.claims["Identifiant ISNI"].should.be.an.Array()
     const claim = item.claims["Identifiant ISNI"][0]
-    claim.value.should.equal('0000000073689743')
+    claim.value.should.equal('0000000121334091')
     claim.references.should.be.an.Array()
     claim.references[0].should.deepEqual({
-      "Source d'import": 'VIAF'
+      "Source d'import": 'BNF'
     })
     claim.references[1].should.deepEqual({
-      'identifiant de la zone': 'intermarc_031',
-      'données source de la zone': '$a 0000000073689743 $2 VIAF $d 20130802'
+      'identifiant de la zone': 'unimarc_010',
+      'données source de la zone': '$a 0000000121334091 $2 BNF $d 20150918'
     })
     done()
   })
