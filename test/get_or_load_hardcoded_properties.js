0a56791307f0cdfede24ed02603ebbd134d7dc7d
jums <jums@inventaire.io>
2019-09-04 18:39:55 +0200

    Ajoute l'analyse de certains types de notices
    en tant que notice générale sans spécification

--- test/get_or_load_hardcoded_properties.js
+++ test/get_or_load_hardcoded_properties.js
@@ -2,7 +2,7 @@ require('should')
 const getOrLoadHardCodedProperties = require('../lib/load/get_or_load_hard_coded_properties')
 
 it('should return an object of hard coded properties', done => {
-  const hardCodedPropertyPseudoId = 'date de consultation'
+  const hardCodedPropertyPseudoId = 'Date de naissance'
   getOrLoadHardCodedProperties()
   .then((hardCodedProperties)=>{
     const hardCodedProperty = hardCodedProperties[hardCodedPropertyPseudoId]
