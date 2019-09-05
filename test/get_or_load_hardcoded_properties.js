5cc7c1dc058883b3083a70848bd6968d993e52a6
maxlath <k@maxlath.eu>
2019-09-05 13:37:52 +0200

    tests: ajout d'une extension de délai manquante

--- test/get_or_load_hardcoded_properties.js
+++ test/get_or_load_hardcoded_properties.js
@@ -1,16 +1,20 @@
 require('should')
 const getOrLoadHardCodedProperties = require('../lib/load/get_or_load_hard_coded_properties')
 
-it('should return an object of hard coded properties', done => {
-  const hardCodedPropertyPseudoId = 'Date de naissance'
-  getOrLoadHardCodedProperties()
-  .then((hardCodedProperties)=>{
-    const hardCodedProperty = hardCodedProperties[hardCodedPropertyPseudoId]
-    hardCodedProperty.should.be.an.Object()
-    hardCodedProperty.datatype.should.equal('time')
-    hardCodedProperty.pseudoId.should.equal(hardCodedPropertyPseudoId)
-    hardCodedProperty.id.should.startWith('P')
-    done()
+describe('get_or_load_hard_coded_properties', function () {
+  this.timeout(20000)
+
+  it('should return an object of hard coded properties', done => {
+    const hardCodedPropertyPseudoId = 'Date de naissance'
+    getOrLoadHardCodedProperties()
+    .then((hardCodedProperties)=>{
+      const hardCodedProperty = hardCodedProperties[hardCodedPropertyPseudoId]
+      hardCodedProperty.should.be.an.Object()
+      hardCodedProperty.datatype.should.equal('time')
+      hardCodedProperty.pseudoId.should.equal(hardCodedPropertyPseudoId)
+      hardCodedProperty.id.should.startWith('P')
+      done()
+    })
+    .catch(done)
   })
-  .catch(done)
 })
